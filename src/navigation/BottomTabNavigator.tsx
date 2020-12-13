3f548d641e9c806a35de6fc7e3f27fdf65065fec
Sanjeev Yadav <sanjeevy133@protonmail.com>
2020-12-13 16:00:13 +0530

    feat: Add localization support

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Text } from 'react-native';
+import { translate } from '../i18n';
 
 const Tab = createBottomTabNavigator();
 
-const HomeScreen = () => <Text>Home</Text>;
+const HomeScreen = () => (
+  <View>
+    <Text>{translate('common.pluralizationExample', { count: 0 })}</Text>
+    <Text>{translate('common.pluralizationExample', { count: 10 })}</Text>
+    <Text>{translate('common.pluralizationExample', { count: 1 })}</Text>
+  </View>
+);
 
 const CategoriesScreen = () => <Text>Category</Text>;
 
@@ -15,10 +23,22 @@ const CartScreen = () => <Text>Cart</Text>;
 const BottomTabNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Category" component={CategoriesScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="Cart" component={CartScreen} />
+      <Tab.Screen
+        name={translate('homeScreen.appbarTitle')}
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        name={translate('categoriesScreen.appbarTitle')}
+        component={CategoriesScreen}
+      />
+      <Tab.Screen
+        name={translate('profileScreen.appbarTitle')}
+        component={ProfileScreen}
+      />
+      <Tab.Screen
+        name={translate('cartScreen.appbarTitle')}
+        component={CartScreen}
+      />
     </Tab.Navigator>
   );
 };

29867b4060107aaf0a789bc74677119e40644294
Sanjeev Yadav <sanjeevy133@protonmail.com>
2020-12-25 18:16:38 +0530

    error

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,23 +1,46 @@
 import React from 'react';
+import { Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import StackNavigator from './StackNavigator';
-import { DrawerScreen } from '../screens';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+const HomeScreen = ({ navigation }): React.ReactElement => (
+  <View>
+    <Button
+      title="Open Alert Dialog"
+      onPress={() =>
+        navigation.navigate('dialog', {
+          dismissible: true,
+          title: 'Hello',
+          description:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It',
+        })
+      }
+    />
+  </View>
+);
+
+const Dialog = () => (
+  <View style={{ flex: 1, backgroundColor: 'rgba(0,0,0,.5)' }}>
+    <Text>Dialog</Text>
+  </View>
+);
 
 const RootNavigator = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator drawerContent={props => <DrawerScreen {...props} />}>
-        <Drawer.Screen
-          name="drawer"
-          component={StackNavigator}
-          options={{
-            swipeEnabled: false,
-          }}
-        />
-      </Drawer.Navigator>
+      <Stack.Navigator
+        mode="modal"
+        headerMode="none"
+        screenOptions={{
+          cardStyle: { backgroundColor: 'transparent' },
+          cardOverlayEnabled: true,
+        }}
+      >
+        <Stack.Screen name={'HomeScreen'} component={HomeScreen} />
+        <Stack.Screen name={'dialog'} component={Dialog} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
